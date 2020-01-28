06427f3b5031541fe95c024014cc49e569c8ae25
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-01-28 15:44:26 +0100

    feat: use openapi v3

--- src/Be.Vlaanderen.Basisregisters.AspNetCore.Swagger.ReDoc/UseSwaggerDocumentationExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.AspNetCore.Swagger.ReDoc/UseSwaggerDocumentationExtensions.cs
@@ -136,9 +136,9 @@ namespace Be.Vlaanderen.Basisregisters.AspNetCore.Swagger.ReDoc
             {
                 apiDocs.UseSwagger(x =>
                 {
-                    x.SerializeAsV2 = true;
-                    x.RouteTemplate = "{documentName}/docs.json";
-                    //for v3: x.RouteTemplate = "swagger/{documentName}/swagger.json";
+                    //x.SerializeAsV2 = true;
+                    //x.RouteTemplate = "{documentName}/docs.json";
+                    x.RouteTemplate = "swagger/{documentName}/swagger.json";
                     x.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
                     {
                         swaggerDoc.Servers = new List<Microsoft.OpenApi.Models.OpenApiServer> { new Microsoft.OpenApi.Models.OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}/" } };
